1ae5946c53797e7f83660a918c2bec41af910969
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-05-16 14:50:07 +0300

    Added socket related includes.

--- src/core/wsgi_os.h
+++ src/core/wsgi_os.h
@@ -2,17 +2,24 @@
 #define _WSGI_OS_H_INCLUDED_
 
 
+#include <arpa/inet.h>
 #include <assert.h>
+#include <errno.h>
+#include <netdb.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/time.h>
+#include <sys/un.h>
 #include <time.h>
 #include <unistd.h>
 
 
-#define wsgi_sleep(s)       sleep(s)
-#define wsgi_msleep(ms)     usleep(ms * 1000)
+#define WSGI_LISTEN_BACKLOG     128
+
+#define wsgi_sleep(s)           sleep(s)
+#define wsgi_msleep(ms)         usleep(ms * 1000)
 
 #endif /* _WSGI_OS_H_INCLUDED_ */
