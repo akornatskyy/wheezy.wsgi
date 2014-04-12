a08ebd635b347888896041d9143e13d459f65103
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2014-04-12 08:06:46 +0300

    Renamed not found module to sample.

---
+++ src/http/modules/wsgi_http_sample_module.c
@@ -0,0 +1,22 @@
+
+#include <wsgi_http.h>
+#include <wsgi_http_sample_module.h>
+
+
+static void *wsgi_http_sample_module_create(wsgi_cycle_t *cycle);
+
+
+wsgi_module_t http_sample_module = {
+    "sample", -1,
+    NULL,
+    wsgi_http_sample_module_create,
+    NULL,
+    NULL
+};
+
+
+static void *
+wsgi_http_sample_module_create(wsgi_cycle_t *cycle)
+{
+    return cycle;
+}
